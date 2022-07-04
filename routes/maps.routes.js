d765f1647d12c2530d1b167f0a82f63348f5c247
Santiago Moncada <santiago.m.a@hotmail.com>
2022-07-04 15:11:41 +0200

    Added more maps routes

--- routes/maps.routes.js
+++ routes/maps.routes.js
@@ -1,10 +1,25 @@
 const router = require("express").Router();
 
+const Map = require("../models/Map.model");
+
 router.get("/", (req, res, next) => {
     res.send("map");
 });
 
 router.get("/create", (req, res, next) => {
-    res.send("map");
+    res.render("maps/create-map");
 });
+
+router.post("/create", (req, res, next) => {
+    const { name, desctiption, address, type } = req.body;
+    const newMap = {
+        name,
+        desctiption,
+        type,
+    }
+    Map.create(req.body)
+        .then(redirect("/maps"))
+        .catch(err => next(err));
+});
+
 module.exports = router;
