282c5e11c4974b773ca3d48d7f1da330fb8c2353
Santiago Moncada <santiago.m.a@hotmail.com>
2022-07-05 17:36:38 +0200

    added reviews

---
+++ routes/reviews.routes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+
+const Review = require("../models/Review.model");
+const Map = require("../models/Map.model");
+
+const { isLoggedIn } = require("../middleware/session-guard");
+//createReview
+
+router.post("/:mapID/create", isLoggedIn, (req, res, next) => {
+    const { mapID } = req.params;
+
+    const newReview = {
+        ...req.body,
+        owner: req.session.currentUser._id
+    }
+
+    Review.create(newReview)
+        .then(review => {
+            return Map.findByIdAndUpdate(mapID, { $push: { reviews: review._id } })
+        })
+        .then(() => res.redirect(`/maps/${mapID}/details`))
+        .catch(err => next(err))
+});
+
+module.exports = router;
